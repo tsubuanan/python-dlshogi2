8c3095ec888fe60c776f7450cd3290d44f26bc3a
tsubuanan <118257936+tsubuanan@users.noreply.github.com>
2024-12-09 13:34:20 +0900

    Update mcts_player.py

--- pydlshogi2/player/mcts_player.py
+++ pydlshogi2/player/mcts_player.py
@@ -40,6 +40,14 @@ QUEUING = -1
 DISCARDED = -2
 # Virtual Loss
 VIRTUAL_LOSS = 1
+TACTICS = {
+    "四間飛車": {"condition": lambda sfen: "R" in sfen.split()[0][6], 
+                "recommendation": "美濃囲いとの組み合わせがおすすめです。"},
+    "中飛車": {"condition": lambda sfen: "R" in sfen.split()[0][4], 
+                "recommendation": "穴熊との組み合わせが有効です。"},
+    "三間飛車": {"condition": lambda sfen: "R" in sfen.split()[0][2], 
+                "recommendation": "高美濃囲いと相性が良いです。"}
+}
 
 # 温度パラメータを適用した確率分布を取得
 def softmax_temperature_with_normalize(logits, temperature):
@@ -195,22 +203,29 @@ class MCTSPlayer(BasePlayer):
             self.queue_node(self.root_board, current_node)
         self.eval_node()
 
+    def suggest_tactics(self, sfen):
+        """
+        現在のSFEN形式の盤面情報を基に戦術を提案するメソッド
+        """
+        for tactic, data in TACTICS.items():
+            if data["condition"](sfen):
+                print(f"info string 戦術提案: {tactic}。{data['recommendation']}")
+                return
+        print("info string 戦術提案: 戦術が特定できません。")
+
     def position(self, sfen, usi_moves):
         if sfen == 'startpos':
             self.root_board.reset()
         elif sfen[:5] == 'sfen ':
-            self.root_board.set_sfen(sfen[5:])
-
+            self.root_board.set_sfen(sfen[5:])# 戦術提案
+        self.suggest_tactics(self.root_board.sfen())  # 現在の盤面を解析して提案
         starting_pos_key = self.root_board.zobrist_hash()
-
         moves = []
         for usi_move in usi_moves:
             move = self.root_board.push_usi(usi_move)
             moves.append(move)
         self.tree.reset_to_position(starting_pos_key, moves)
 
-        if self.debug:
-            print(self.root_board)
 
     def set_limits(self, btime=None, wtime=None, byoyomi=None, binc=None, winc=None, nodes=None, infinite=False, ponder=False):
         # 探索回数の閾値を設定
